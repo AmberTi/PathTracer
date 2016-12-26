72f4038e0d63f0bdf1bd00da94c25da4606debc4
AmberTi <amber.tillemans@outlook.com>
2016-12-26 11:50:31 +0100

    Cleaning XML to text only
    Before: first delete double space, then punctuation.
    Now : first delete punctuation, then double space.
    Result : "clean" data (text only)

--- PathTrace_PTL.py
+++ PathTrace_PTL.py
@@ -5,17 +5,19 @@ f = open("Guiltless_49v50.xml","r")
 data = f.read()
 tags = re.compile(r"<[^>]*>")
 notags = tags.sub("", data) #still dirty
-dirt = re.compile(r"(-->)| (&amp;) | (xx+) | *\s+") #erase common dirt
-notags_clean = dirt.sub("", notags)
-clean_data = " ".join(notags_clean.split()) #erase double spacing 
-"""print(type(clean_data)) """
+dirt = re.compile(r"(-->)| (&amp;) | (\*\s)+") #erase common dirt
+data_notags = dirt.sub("", notags)
 
-
-punct_list = [",",".","-","!","?",":"] 
-clean_data_nopunct = [] 
-for character in clean_data: 
+punct_list = [",",".","-","!","?",":", "(", ")"] 
+nopunct = [] 
+for character in data_notags: 
 	if character in punct_list: 
 		character = ""
 	else: 
-		clean_data_nopunct.append(character)
-print("".join(clean_data_nopunct))
+		nopunct.append(character)
+data_notags_nopunct = ("".join(nopunct))
+
+
+double_spacing = re.compile(r"\s+ | \n+")
+data_notags_nopunct_nospace = double_spacing.sub(" ", data_notags_nopunct)
+print(data_notags_nopunct_nospace)
